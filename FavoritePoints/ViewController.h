2092cb995e4ddd112066d48610f8bbe667f3d389
Alexander Kozin <alexander@kozin.me>
2017-08-26 20:09:51 +0300

    Initial Commit

---
+++ FavoritePoints/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FavoritePoints
+//
+//  Created by Alexander Kozin on 26.08.17.
+//  Copyright © 2017 Pragmatic Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FavoritePoints/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FavoritePoints
+//
+//  Created by Alexander Kozin on 26.08.17.
+//  Copyright © 2017 Pragmatic Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
